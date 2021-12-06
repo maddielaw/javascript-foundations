7623209dd1dafcb33923f573cf610d7333f7e719
Madison Law <madisonlaw1@gmail.com>
2021-12-06 16:37:01 -0700

    Add pirate tests setup and robShip

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,19 @@
+class Pirate {
+    constructor(name, job) {
+        this.name = name;
+        if (job === undefined) {
+            this.job = "scallywag"
+        } else {
+            this.job = job;
+        };
+        this.cursed = false;
+        this.booty = 0;
+    }
+    robShip() {
+        this.booty = this.booty + 100;
+        return "YAARRR!"
+    }
+};
+
+
+module.exports = Pirate
